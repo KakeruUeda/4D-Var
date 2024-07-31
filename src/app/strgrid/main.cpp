dbc6bb821efb460dce2e69aa4bd81366fcc9107c
KakeruUeda <kakecoco2580@icloud.com>
2024-07-31 09:30:07 +0100

    seeing the snapshot at t=0 as the initial velocity field must be a better approach

--- src/app/strgrid/main.cpp
+++ src/app/strgrid/main.cpp
@@ -22,36 +22,8 @@ void setControlBoundary(Cell &cell, ControlBoundary &controlBoundary,
                         std::vector<std::vector<int>> &controlNodeInCell,
                         std::vector<int> &controlCellMap,
                         int nxNodes, int nyNodes, int nzNodes,
-                        int nxCells, int nyCells, int nzCells)
-{
-   int nNodesInCell = 8;
-    if(controlBoundary == ControlBoundary::left){
-        for(int k=0; k<nzNodes; k++){
-            for(int j=0; j<nyNodes; j++){
-                for(int i=0; i<nxNodes; i++){
-                    if(i == 0){
-                        controlBoundaryMap.push_back(i+j*nxNodes+k*nxNodes*nyNodes);
-                    }
-                }
-            }
-        }
-        for(int k=0; k<nzCells; k++){
-            for(int j=0; j<nyCells; j++){
-                for(int i=0; i<nxCells; i++){
-                    if(i == 0){
-                        std::vector<int> vecTmp(4, 0);
-                        vecTmp[0] = cell(i+j*nxCells+k*nxCells*nyCells).node[0];
-                        vecTmp[1] = cell(i+j*nxCells+k*nxCells*nyCells).node[3];
-                        vecTmp[2] = cell(i+j*nxCells+k*nxCells*nyCells).node[7];
-                        vecTmp[3] = cell(i+j*nxCells+k*nxCells*nyCells).node[4];
-                        controlNodeInCell.push_back(vecTmp);
-                        controlCellMap.push_back(i+j*nxCells+k*nxCells*nyCells);
-                    }
-                }
-            }
-        }
-    }
-}
+                        int nxCells, int nyCells, int nzCells);
+
 
 int main(int argc, char* argv[])
 { 
@@ -175,4 +147,41 @@ int main(int argc, char* argv[])
     }
 
     return EXIT_SUCCESS;
+}
+
+
+void setControlBoundary(Cell &cell, ControlBoundary &controlBoundary, 
+                        std::vector<int> &controlBoundaryMap,
+                        std::vector<std::vector<int>> &controlNodeInCell,
+                        std::vector<int> &controlCellMap,
+                        int nxNodes, int nyNodes, int nzNodes,
+                        int nxCells, int nyCells, int nzCells)
+{
+   int nNodesInCell = 8;
+    if(controlBoundary == ControlBoundary::left){
+        for(int k=0; k<nzNodes; k++){
+            for(int j=0; j<nyNodes; j++){
+                for(int i=0; i<nxNodes; i++){
+                    if(i == 0){
+                        controlBoundaryMap.push_back(i+j*nxNodes+k*nxNodes*nyNodes);
+                    }
+                }
+            }
+        }
+        for(int k=0; k<nzCells; k++){
+            for(int j=0; j<nyCells; j++){
+                for(int i=0; i<nxCells; i++){
+                    if(i == 0){
+                        std::vector<int> vecTmp(4, 0);
+                        vecTmp[0] = cell(i+j*nxCells+k*nxCells*nyCells).node[0];
+                        vecTmp[1] = cell(i+j*nxCells+k*nxCells*nyCells).node[3];
+                        vecTmp[2] = cell(i+j*nxCells+k*nxCells*nyCells).node[7];
+                        vecTmp[3] = cell(i+j*nxCells+k*nxCells*nyCells).node[4];
+                        controlNodeInCell.push_back(vecTmp);
+                        controlCellMap.push_back(i+j*nxCells+k*nxCells*nyCells);
+                    }
+                }
+            }
+        }
+    }
 }
